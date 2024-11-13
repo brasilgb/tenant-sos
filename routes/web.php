3b8dcad00e7bef9afc87e34545018ceb4d6f434a
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-11-13 12:57:03 -0300

    push

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::domain('{localhost')->group(function () {
+Route::domain('{tenantsos')->group(function () {
     Route::get('/', function (string $tenant) {
         return Inertia::render('Welcome');
     });
 });
-Route::domain('{tenant}.localhost')->group(function () {
+Route::domain('{tenant}.tenantsos')->group(function () {
     Route::get('/', function (string $tenant) {
         return Inertia::render('Welcome');
     });
